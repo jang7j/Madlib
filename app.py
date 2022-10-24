e3243d50601b53371e9cb438cac4cdfd7d0c99b0
Yun Jang <jang7j@yahoo.com>
2022-10-24 15:15:39 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-import random
-def guess(x):
-    random_num = random.randint(1, x)
-    guess = 0
-    while guess != random_num:
-        guess = input(f'Guess a number between 1 and {x}')
-        if guess < random_num:
-            print('sorry guess again. too low')
-        else guess > random_num:
-        p
-guess(10)
+adjective1 = input("Give me an adjective: ")
+adjective2 = input("Give me another adjective: ")
+verb1 = input("Give me a verb: ")
+verb2 = input("Give me another verb: ")
+noun = input("Give me a noun: ")
+
+madlib = f"Hello there! This is a story about a dog named Gus. He has a/an {adjective1} personality that is super infectious. He likes to {verb1} his tail and give you a hug when he senses that you are sad. However, he does not like to {verb2} and may bark arrogantly if you step onto his personal space. Overall, he is one great {adjective2} blue-eyed {noun}."
+
+print(madlib)
